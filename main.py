9ee172c9c1ce81871e9a1a9a2f639dfaecc62431
Kacper Kielak <kielak.kacper@gmail.com>
2018-03-29 20:29:28 +0200

    Split data into test and train sets

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import numpy as np
 
 
-USED_DTYPE = np.float64
 DATA_PATH = './data.txt'
 FEATURES = ('Diameter', 'Model', 'Year', '?')
 OUTPUTS = ('Price',)
+
+USED_DTYPE = np.float64
+TEST_DATA_SIZE = 0.15  # What part of the whole data should be set aside for testing
+
+
+def get_test_train_data(data, test_size):
+    """
+    Splits the data into 2 separate datasets, train set and test set
+    :param data: Data to split
+    :param test_size: What part of the whole data should be taken for the test data
+    :return: Data sampled randomly into test and train data.
+    """
+    data_copy = data[:]  # copy to avoid mutation of data parameter and make the whole method immutable
+    np.random.shuffle(data_copy)
+    test_end_index = int(test_size * len(data))
+    test_data = data_copy[:test_end_index]
+    train_data = data_copy[test_end_index:]
+    return test_data, train_data
 
 
 def normalize_data(data, min_bound=-1, max_bound=1):
@@ -38,7 +55,20 @@ with open(DATA_PATH, 'r') as data_file:
 
         data.append(datapoint)
 
-data = np.array(data, dtype=USED_DTYPE)
-data = np.array([normalize_data(column) for column in data.T]).T
 
-print(data)
+data = np.array(data, dtype=USED_DTYPE)
+data = np.array([normalize_data(column) for column in data.T])
+inputs = data[:len(FEATURES)].T
+labels = data[len(FEATURES):].T
+
+test_inputs, train_inputs = get_test_train_data(inputs, TEST_DATA_SIZE)
+test_labels, train_labels = get_test_train_data(labels, TEST_DATA_SIZE)
+
+print('test inputs')
+print(test_inputs)
+print('test labels')
+print(test_labels)
+print('train inputs')
+print(train_inputs)
+print('train_labels')
+print(train_labels)
