Fix test data leaking during normalization

main.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from models.feedforward_nn import FeedforwardNN
 from models.linear_regression import LinearRegression
-from preprocessing import get_test_train_data, normalize_data
+from preprocessing import get_test_train_data, normalize_train_test_data
 
 DATA_PATH = './data.txt'
 FEATURES = ('Diameter', 'Model', 'Year', '?')
@@ -10,12 +10,13 @@ OUTPUTS = ('Price',)
 
 USED_DTYPE = np.float64
 TEST_DATA_SIZE = 0.15  # What part of the whole data should be set aside for testing
-TRAIN_STEPS = 100000  # How many train steps to perform, at the moment each train step uses whole training data
+TRAIN_STEPS = 500000  # How many train steps to perform, at the moment each train step uses whole training data
 LOGGING_FREQUENCY = 10000  # How often to log training data
 
 SOLVABLE_MODELS = (
     LinearRegression(len(FEATURES), len(OUTPUTS), name='linear_regression', dtype=USED_DTYPE),
 )
+
 TRAINING_MODELS = (
     FeedforwardNN(len(FEATURES), [720], len(OUTPUTS), activation=tf.nn.leaky_relu,
                   is_regression=True, name='overfitting_feedforward', dtype=USED_DTYPE),
@@ -53,8 +54,8 @@ with open(DATA_PATH, 'r') as data_file:
 
 # Prepare data
 data = np.array(data, dtype=USED_DTYPE)
-data = np.array([normalize_data(column) for column in data.T]).T
 train_data, test_data = get_test_train_data(data, TEST_DATA_SIZE)
+train_data, test_data, normalization_parameters = normalize_train_test_data(train_data, test_data)
 
 train_inputs = train_data[:, :len(FEATURES)]
 train_labels = train_data[:, len(FEATURES):]

preprocessing.py
@@ -16,21 +16,52 @@ def get_test_train_data(data, test_size):
     return train_data, test_data
 
 
-def normalize_data(data, min_bound=0, max_bound=100):
+def normalize_train_test_data(train, test):
     """
-    Normalizes data.
-    :param data: 1D collection to normalize.
-    :param min_bound: Minimal bound for the data normalization.
-    :param max_bound: Maximal bound for the data normalization.
-    :return: Normalized data in between min_bound and max_bound, in the same format as input data.
+    Normalizes 2 matrices of data, train and test making sure that normalization parameters are decided based
+    only on train data to avoid leaking any information about test data to the model.
+    :param train: Train data (as numpy array), second axis (axis=1 in numpy) should correspond to separate features.
+    :param test: Test data (as numpy array), second axis (axis=1 in numpy) should correspond to separate features.
+    :return: Normalized train data, normalized test data, and 2D array of normalization parameters. For each feature
+    there are different normalization parameters and each row of normalization parameters consists of 2 variables:
+    scale_coefficient and mean_change where normalized_value = scale_coefficient*normalized_value - mean_change
+    (normalization parameters are decided purely based on the train data).
     """
-    if min_bound >= max_bound:
-        raise ValueError('Minimal bound ({}) can\'t be lower than maximal bound ({})'.format(min_bound, max_bound))
+    normalized_train_data = []
+    normalization_parameters = []
+    for column in train.T:
+        normalized_column, scale_coefficient, mean_change = normalize_data_vector(column)
+        normalized_train_data.append(normalized_column)
+        normalization_parameters.append((scale_coefficient, mean_change))
 
-    bound_range = max_bound - min_bound
+    normalized_test_data = []
+    for i, column in enumerate(test.T):
+        scale_coeff = normalization_parameters[i][0]
+        mean = normalization_parameters[i][1]
+        normalized_column, _, _ = normalize_data_vector(column, scale_coefficient=scale_coeff, mean_change=mean)
+        normalized_test_data.append(normalized_column)
+
+    return np.array(normalized_train_data).T, np.array(normalized_test_data).T, normalization_parameters
+
+
+def normalize_data_vector(data, scale_coefficient=None, mean_change=None):
+    """
+    Normalizes data based on given (or calculated based on the given data) normalization parameters
+    :param data: 1D collection to normalize.
+    :param scale_coefficient: Used to multiply all values (used to scale the data). If set to None calculated so all
+    of the values after scaling perfectly fit into the range of 100.
+    :param mean_change: Added to all of the values (used to change the mean of the data). If set to None calculated
+    so the minimum value in the dataset after scaling is equal to 0.
+    :return: Normalized data based on scale_coefficient and mean_chage in the same format as input data and
+    where normalized_data = scale_coefficient*data - mean_change
+    """
     min_value = min(data)
-    max_value = max(data)
-    data_range = max_value - min_value
-    scale_coefficient = bound_range / data_range
-    mean_change = min_bound - (min_value * scale_coefficient)
-    return [scale_coefficient*value + mean_change for value in data]
+    if scale_coefficient is None:
+        max_value = max(data)
+        data_range = max_value - min_value
+        scale_coefficient = 100 / data_range
+
+    if mean_change is None:
+        mean_change = (min_value * scale_coefficient)
+
+    return [scale_coefficient*value - mean_change for value in data], scale_coefficient, mean_change